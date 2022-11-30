009f3c3eecc8b8c7445fc2e4e971a094de3d130f
Joo-Byungho <kjcoco13@gmail.com>
2022-11-30 11:29:21 +0900

    Add example of cron job remarked

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -2,4 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 
 @Injectable()
-export class CronService {}
+export class CronService {
+  // every 10 seconds
+  // @Cron('*/10 * * * * *')
+  // show() {
+  //   console.log('show');
+  // }
+}
