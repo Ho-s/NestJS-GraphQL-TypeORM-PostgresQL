71740b2c645f0b84a6d5636e904dd4847b20f74c
Joo-Byungho <kjcoco13@gmail.com>
2022-11-30 15:11:06 +0900

    Remove useleess util

--- src/util/bigintTransformation.ts
+++
@@ -1,7 +0,0 @@
-import { ValueTransformer } from 'typeorm';
-
-export const bigintTransformation: ValueTransformer = {
-  to: (entityValue: number) => entityValue?.toString(),
-  from: (databaseValue: string): number =>
-    databaseValue ? parseInt(databaseValue, 10) : null,
-};
