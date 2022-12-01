a39f2727d15f1855c3e42e7e83c295d293503328
Joo-Byungho <kjcoco13@gmail.com>
2022-12-02 02:39:54 +0900

    Fix auth logic
    - Install jwt dependency
    - Get private key to be in .env
    - Change logic of passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from '../user/user.module';
+import { UserModule } from 'src/user/user.module';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService, AuthResolver, JwtStrategy],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '1d',
+        },
+      }),
+    }),
+    ConfigModule,
+    UserModule,
+  ],
+  providers: [AuthResolver, AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,22 @@
 import { JwtWithUser } from '../entities/auth';
 import { UserService } from '../user/user.service';
-import { generateJWT } from './util/generateJWT';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { SignInInput, SignUpInput } from 'src/entities/auth/auth.input';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { pick } from 'lodash';
+import { User } from 'src/entities';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  signJWT(user: User) {
+    return this.jwtService.sign(pick(user, ['id', 'role']));
+  }
 
   async signUp(input: SignUpInput): Promise<JwtWithUser> {
     const doesExistId = await this.userService.getOne({
@@ -23,7 +32,7 @@ export class AuthService {
       ...input,
     });
 
-    const jwt = generateJWT(user);
+    const jwt = this.signJWT(user);
 
     return { jwt, ...user };
   }
@@ -48,7 +57,7 @@ export class AuthService {
       throw new BadRequestException('Password is incorrect');
     }
 
-    const jwt = generateJWT(user);
+    const jwt = this.signJWT(user);
 
     return { jwt, ...user };
   }
