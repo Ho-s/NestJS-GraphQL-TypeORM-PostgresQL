fe1717e56f675becd42f889b4c97632328bb01d1
Joo-Byungho <kjcoco13@gmail.com>
2022-12-07 16:53:18 +0900

    Divide use facotries from app module

---
+++ src/modules/shared/services/setting.service.ts
@@ -0,0 +1,92 @@
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import GraphQLJSON from 'graphql-type-json';
+import { isNil } from 'lodash';
+import { join } from 'path';
+import { formatError } from 'src/modules/format/graphql-error.format';
+
+@Injectable()
+export class SettingService {
+  constructor(private readonly configService: ConfigService) {}
+
+  private get(key: string): string {
+    const value = this.configService.get<string>(key);
+
+    if (isNil(value)) {
+      throw new Error(key + ' environment variable does not set');
+    }
+
+    return value;
+  }
+
+  private getNumber(key: string): number {
+    const value = this.get(key);
+
+    try {
+      return Number(value);
+    } catch {
+      throw new Error(key + ' environment variable is not a number');
+    }
+  }
+
+  private getString(key: string): string {
+    const value = this.get(key);
+
+    return value.replace(/\\n/g, '\n');
+  }
+
+  get nodeEnv(): string {
+    return this.getString('NODE_ENV');
+  }
+
+  get isDevelopment(): boolean {
+    return this.nodeEnv === 'development';
+  }
+
+  get isProduction(): boolean {
+    return this.nodeEnv === 'production';
+  }
+
+  get graphqlUseFactory():
+    | Omit<ApolloDriverConfig, 'driver'>
+    | (Promise<Omit<ApolloDriverConfig, 'driver'>> & { uploads: boolean }) {
+    return {
+      uploads: false,
+      resolvers: { JSON: GraphQLJSON },
+      autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
+      sortSchema: true,
+      playground: false,
+      ...(!this.isProduction && {
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      }),
+      context: ({ req }) => ({ req }),
+      cors: {
+        origin: '*',
+        Credential: true,
+      },
+      cache: 'bounded',
+      formatError,
+    };
+  }
+
+  get TypeOrmUseFactory():
+    | TypeOrmModuleOptions
+    | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres' as any,
+      host: this.getString('DB_HOST'),
+      port: this.getNumber('DB_PORT'),
+      username: this.getString('DB_USER'),
+      password: this.getString('DB_PASSWORD'),
+      database: this.getString('DB_NAME'),
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      timezone: '+09:00',
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: false, // if you want to see the query log, change to true
+    };
+  }
+}
---
+++ src/modules/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { SettingService } from './services/setting.service';
+
+@Module({
+  providers: [SettingService],
+  exports: [SettingService],
+})
+export class SharedModule {}
