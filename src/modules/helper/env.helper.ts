f7737a9e671f7fa2ac9f63547c693170aa596af7
Joo-Byungho <kjcoco13@gmail.com>
2022-12-12 16:47:28 +0900

    Add dot

--- src/modules/helper/env.helper.ts
+++ src/modules/helper/env.helper.ts
@@ -2,6 +2,6 @@ import { resolve } from 'path';
 
 export function getEnvPath(dest: string): string {
   const env: string | undefined = process.env.NODE_ENV;
-  const filename: string = env ? `${env}.env` : 'development.env';
+  const filename: string = env ? `.${env}.env` : '.development.env';
   return resolve(`${dest}/${filename}`);
 }
