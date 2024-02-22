bd1a6eb00325a0a893b8c0484b88aebbb34cf635
Joo-Byungho <kjcoco13@gmail.com>
2024-02-22 17:22:34 +0900

    Refactor: Remove place entity

--- src/place/place.repositoy.ts
+++
@@ -1,6 +0,0 @@
-import { CustomRepository } from '../common/decorators/typeorm.decorator';
-import { Place } from './entities/place.entity';
-import { ExtendedRepository } from 'src/common/graphql/customExtended';
-
-@CustomRepository(Place)
-export class PlaceRepository extends ExtendedRepository<Place> {}
