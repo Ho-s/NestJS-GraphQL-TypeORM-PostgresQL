7f6cefcced352a53c96d40a132e57ecf8710a4f1
Joo-Byungho <kjcoco13@gmail.com>
2024-05-09 17:00:15 +0900

    Refactor: Add this type in function
    - Remove no used import

--- src/common/graphql/customExtended.ts
+++ src/common/graphql/customExtended.ts
@@ -5,7 +5,6 @@ import {
   FindManyOptions,
   FindOneOptions,
   FindOptionsOrder,
-  FindOptionsSelect,
   Repository,
 } from 'typeorm';
 
@@ -24,7 +23,10 @@ import {
 import { getConditionFromGqlQuery } from './utils/getConditionFromGqlQuery';
 import { processWhere } from './utils/processWhere';
 
-export function filterOrder<T>(order: FindOptionsOrder<T>) {
+export function filterOrder<T>(
+  this: Repository<T>,
+  order: FindOptionsOrder<T>,
+) {
   Object.entries(order).forEach(([key, value]: [string, ISort]) => {
     if (!(key in this.metadata.propertiesMap)) {
       throw new BadRequestException(
