509c0a8057abca861852b67e951a51ab8420a764
Joo-Byungho <kjcoco13@gmail.com>
2024-06-05 17:58:33 +0900

    Fix: Select issue (#26)
    - Add metadata to resolve select issue

--- src/common/graphql/utils/getConditionFromGqlQuery.ts
+++ src/common/graphql/utils/getConditionFromGqlQuery.ts
@@ -1,5 +1,6 @@
 import { parse, print } from 'graphql';
 import { set } from 'lodash';
+import { Repository } from 'typeorm';
 
 import { AddKeyValueInObjectProps, GetInfoFromQueryProps } from './types';
 
@@ -32,10 +33,11 @@ const addKeyValuesInObject = <Entity>({
   return { relations, select };
 };
 
-export const getConditionFromGqlQuery = <Entity>(
+export function getConditionFromGqlQuery<Entity>(
+  this: Repository<Entity>,
   query: string,
   hasCountType?: boolean,
-): GetInfoFromQueryProps<Entity> => {
+): GetInfoFromQueryProps<Entity> {
   const ast = parse(query);
   const operationJson = print(ast);
 
@@ -52,12 +54,21 @@ export const getConditionFromGqlQuery = <Entity>(
   const stack = [];
 
   const regex = /[\s\{]/g;
+  let lastMetadata = this.metadata;
 
   return splitted.reduce(
     (acc, line) => {
       const replacedLine = line.replace(regex, '');
+
       if (line.includes('{')) {
         stack.push(replacedLine);
+        const isFirstLineDataType = hasCountType && replacedLine === DATA;
+
+        if (!isFirstLineDataType) {
+          lastMetadata = lastMetadata.relations.find(
+            (v) => v.propertyName === replacedLine,
+          ).inverseEntityMetadata;
+        }
 
         return addKeyValuesInObject({
           stack,
@@ -67,13 +78,33 @@ export const getConditionFromGqlQuery = <Entity>(
           hasCountType,
         });
       } else if (line.includes('}')) {
+        const hasDataTypeInStack =
+          hasCountType && stack.length && stack[0] === DATA;
+
+        lastMetadata =
+          stack.length < (hasDataTypeInStack ? 3 : 2)
+            ? this.metadata
+            : lastMetadata.relations.find(
+                (v) => v.propertyName === stack[stack.length - 2],
+              ).inverseEntityMetadata;
+
         stack.pop();
 
         return acc;
       }
 
+      const addedStack = [...stack, replacedLine];
+
+      if (
+        ![...lastMetadata.columns, ...lastMetadata.relations]
+          .map((v) => v.propertyName)
+          .includes(addedStack[addedStack.length - 1])
+      ) {
+        return acc;
+      }
+
       return addKeyValuesInObject({
-        stack: [...stack, replacedLine],
+        stack: addedStack,
         relations: acc.relations,
         select: acc.select,
         hasCountType,
@@ -84,4 +115,4 @@ export const getConditionFromGqlQuery = <Entity>(
       select: {},
     },
   );
-};
+}
