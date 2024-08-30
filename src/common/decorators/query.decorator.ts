62a54f6315c19426b929b8ff01499cffbaaa3c62
Joo-Byungho <kjcoco13@gmail.com>
2024-08-30 15:57:32 +0900

    Fix: current query error

--- src/common/decorators/query.decorator.ts
+++ src/common/decorators/query.decorator.ts
@@ -6,39 +6,38 @@ import { parse, print } from 'graphql';
 export const CurrentQuery = createParamDecorator<string[]>(
   (_, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-    const { fieldName } = context.getArgs()[3];
+    const { fieldName, path } = context.getArgs()[3];
 
     const query = ctx.getContext().req.body.query;
     const operationJson = print(parse(query));
     const operationArray = operationJson.split('\n');
 
-    let depth = -1;
-    const operation = operationArray.reduce((acc: string[], cur, _, arr) => {
-      if (depth === 0) {
-        arr.splice(1); //eject
-        return acc;
-      }
+    operationArray.shift();
+    operationArray.pop();
 
-      if (cur.includes(fieldName)) {
+    const firstLineFinder = operationArray.findIndex((v) =>
+      v.includes(fieldName === path.key ? fieldName : path.key + ':'),
+    );
+
+    operationArray.splice(0, firstLineFinder);
+
+    const stack = [];
+
+    let depth = 0;
+
+    for (const line of operationArray) {
+      stack.push(line);
+      if (line.includes('{')) {
         depth++;
-      }
-
-      if (depth < 0) {
-        return acc;
-      }
-
-      if (cur.includes('{')) {
-        depth++;
-      }
-
-      if (cur.includes('}')) {
+      } else if (line.includes('}')) {
         depth--;
       }
 
-      acc.push(cur);
-      return acc;
-    }, []);
+      if (depth === 0) {
+        break;
+      }
+    }
 
-    return operation.join('\n');
+    return stack.join('\n');
   },
 );
