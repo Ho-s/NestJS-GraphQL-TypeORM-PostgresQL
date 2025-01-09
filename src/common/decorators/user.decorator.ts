9701ea3429ccb06f73b03e466819c7aae742b926
Joo-Byungho <kjcoco13@gmail.com>
2025-01-09 18:05:16 +0900

    Chore

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -2,7 +2,7 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
+  (_: unknown, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req.user;
   },
