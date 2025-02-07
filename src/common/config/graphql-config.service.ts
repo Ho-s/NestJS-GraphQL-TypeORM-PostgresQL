00a928f92df551bc49535583e36d515805b0a8c5
Joo-Byungho <kjcoco13@gmail.com>
2025-02-07 19:28:53 +0900

    Refactor: Replace SettingModule with GraphqlConfigService for GraphQL configuration

---
+++ src/common/config/graphql-config.service.ts
@@ -0,0 +1,43 @@
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GqlOptionsFactory } from '@nestjs/graphql';
+
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default';
+import GraphQLJSON from 'graphql-type-json';
+import { join } from 'path';
+import { cwd } from 'process';
+
+import { formatError } from '../format/graphql-error.format';
+
+@Injectable()
+export class GraphqlConfigService
+  implements GqlOptionsFactory<ApolloDriverConfig>
+{
+  constructor(private readonly configService: ConfigService) {}
+
+  createGqlOptions(): Promise<ApolloDriverConfig> | ApolloDriverConfig {
+    return {
+      // uploads: false,
+      resolvers: { JSON: GraphQLJSON },
+      autoSchemaFile: join(
+        cwd(),
+        `${this.configService.get('NODE_ENV') === 'test' ? 'test' : 'src'}/graphql-schema.gql`,
+      ),
+      sortSchema: true,
+      playground: false,
+      plugins: [
+        this.configService.get('NODE_ENV') === 'production'
+          ? ApolloServerPluginLandingPageProductionDefault()
+          : ApolloServerPluginLandingPageLocalDefault(),
+      ],
+
+      context: ({ req }) => ({ req }),
+      cache: 'bounded',
+      formatError,
+    };
+  }
+}
--- src/common/shared/setting/setting.service.ts
+++ src/common/shared/setting/setting.service.ts
@@ -1,36 +1,8 @@
-import { ApolloDriverConfig } from '@nestjs/apollo';
 import { Injectable } from '@nestjs/common';
-
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import GraphQLJSON from 'graphql-type-json';
-import { join } from 'path';
-
-import { formatError } from 'src/common/format/graphql-error.format';
 
 import { UtilService } from '../services/util.service';
 
 @Injectable()
 export class SettingService {
   constructor(private readonly utilService: UtilService) {}
-
-  get graphqlUseFactory():
-    | Omit<ApolloDriverConfig, 'driver'>
-    | (Promise<Omit<ApolloDriverConfig, 'driver'>> & { uploads: boolean }) {
-    return {
-      uploads: false,
-      resolvers: { JSON: GraphQLJSON },
-      autoSchemaFile: join(
-        process.cwd(),
-        `${this.utilService.nodeEnv === 'test' ? 'test' : 'src'}/graphql-schema.gql`,
-      ),
-      sortSchema: true,
-      playground: false,
-      ...(!this.utilService.isProduction && {
-        plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      }),
-      context: ({ req }) => ({ req }),
-      cache: 'bounded',
-      formatError,
-    };
-  }
 }
