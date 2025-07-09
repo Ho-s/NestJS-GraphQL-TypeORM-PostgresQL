8bcf41cb1087e1a3f2a0be392a0e489548db4d9e
Joo-Byungho <kjcoco13@gmail.com>
2025-07-09 12:31:54 +0900

    chore: remove no use

--- src/common/config/graphql-config.service.ts
+++ src/common/config/graphql-config.service.ts
@@ -1,5 +1,5 @@
 import { ApolloDriverConfig } from '@nestjs/apollo';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GqlOptionsFactory } from '@nestjs/graphql';
 
